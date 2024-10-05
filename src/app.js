42396af51b0a23b142fc89fa7a1a9d87a1fea1d9
Esteban Salazar <estebansalazar1407@gmail.com>
2024-10-05 16:51:24 -0500

    "ApiRest con nodeJs, express y MongoDB"

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoutes');
+const errorHandler = require('./middlewares/errorHandler');
+
+const app = express();
+
+// Middleware global
+app.use(cors());
+app.use(express.json());
+app.use('/api', userRoutes);
+app.use(errorHandler);
+
+module.exports = app;
---
+++ src/controllers/userControllers.js
@@ -0,0 +1,23 @@
+const User = require('../models/User');
+
+// Obtener todos los usuarios
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: 'Error al obtener usuarios' });
+  }
+};
+
+// Crear un nuevo usuario
+exports.createUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const newUser = new User({ name, email, password });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: 'Error al crear usuario' });
+  }
+};
